ce5b991a1705513b04470d98ab64e354e2555f9e
Kirill Dudchenko <kirill@MacBook-Air-Kirill.local>
2022-06-23 20:50:19 +0300

    minor changes

--- perception-box/src/components/PreviewCard.js
+++ perception-box/src/components/PreviewCard.js
@@ -4,12 +4,12 @@ export default function Previewstyles({ data, close }) {
     <div>
       <button onClick={close}>CLOSE</button>
     <ul>
-        <li>{data.name}</li>
-        <li>{data.species}</li>
-        <li>{data.gender}</li>
-        <li>{data.location.name}</li>
-        <li>{data.status}</li>
-        <li>{data.created}</li>
+        <li>Name: {data.name}</li>
+        <li>Species: {data.species}</li>
+        <li>Gender: {data.gender}</li>
+        <li>Location: {data.location.name}</li>
+        <li>Status: {data.status}</li>
+        <li>Created: {data.created}</li>
         <li>Episode:
             <ol>
                 {data.episode.map(item=><li>{item}</li>)}
